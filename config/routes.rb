5ccc15eb4936199a2f1582e1f83df99963b9a24b
maximx <maxim7667@outlook.com>
2013-04-10 11:13:22 +0800

    add user member feature by devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Forum::Application.routes.draw do
+  devise_for :users
+
   resources :boards do
     resources :posts
   end
@@ -53,6 +55,7 @@ Forum::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
+  root :to => "boards#index"
 
   # See how all your routes lay out with "rake routes"
 
