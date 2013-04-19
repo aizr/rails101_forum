8a65e08bed4f2ee5b2c45eb862e5cd12b5a620f7
maximx <maxim7667@outlook.com>
2013-04-19 13:51:42 +0800

    add unicorn.rb

---
+++ config/unicorn.rb
@@ -0,0 +1,9 @@
+#stderr_path "/Users/techbang/Projects/rails101_forum/log/unicorn.log"
+#stdout_path "/Users/techbang/Projects/rails101_forum/log/unicorn.log"
+#pid "/Users/techbang/Projects/rails101_forum/tmp/pids/unicorn.pid"
+stderr_path "/home/apps/rails101_forum/current/log/unicorn.err.log"
+stdout_path "/home/apps/rails101_forum/current/log/unicorn.out.log"
+pid "/home/apps/rails101_forum/current/tmp/pids/unicorn.pid"
+
+worker_processes 2
+listen 2007
