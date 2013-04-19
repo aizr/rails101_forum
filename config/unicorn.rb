5e0b59794c455d58b70ea7cd7c4429cbe047689a
maximx <maxim7667@outlook.com>
2013-04-19 14:20:44 +0800

    modify project_dir path for production, add unicorn gem

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,9 +1,10 @@
-#stderr_path "/Users/techbang/Projects/rails101_forum/log/unicorn.log"
-#stdout_path "/Users/techbang/Projects/rails101_forum/log/unicorn.log"
-#pid "/Users/techbang/Projects/rails101_forum/tmp/pids/unicorn.pid"
-stderr_path "/home/apps/rails101_forum/current/log/unicorn.err.log"
-stdout_path "/home/apps/rails101_forum/current/log/unicorn.out.log"
-pid "/home/apps/rails101_forum/current/tmp/pids/unicorn.pid"
-
 worker_processes 2
-listen 2007
+
+project_dir = "/home/apps/rails101_forum/current"
+working_directory project_dir
+
+stderr_path "#{project_dir}/log/unicorn.err.log"
+stdout_path "#{project_dir}/log/unicorn.out.log"
+pid "#{project_dir}/tmp/pids/unicorn.pid"
+
+listen "/home/apps/rails101_forum/shared/sockets/unicorn.sock"
